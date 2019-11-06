753d430914bf49ceb4a92a2977a401b44b162ea6
JaMo42 <jakob@mohrbacher.eu>
2019-11-06 10:04:49 +0100

    add check for file existence

--- include_guard.cpp
+++ include_guard.cpp
@@ -1,9 +1,12 @@
-#include <iostream>  // std::cout, std::cerr
-#include <string>  // std::string
-#include <fstream>  // std::ofstream
-#include <cctype>  // std::isalpha
+#include <iostream>  // for std::cout, std::cerr
+#include <string>  // for std::string
+#include <fstream>  // for std::ofstream
+#include <cctype>  // for std::isalpha
+#include <string_view>  // for std::string_view
+#include <filesystem>  // for std::filesystem::exists, std::filesystem::path
+namespace fs = std::filesystem;
 
-std::string get_macro_name(const std::string& file_name) {
+std::string get_macro_name(std::string_view file_name) {
     std::string macro_name;
     macro_name.reserve(file_name.length());
     for (const char& character : file_name) {
@@ -25,34 +28,41 @@ int main(const int argc, const char **argv) {
     unsigned num_fails = 0;
     std::cout << "Creating include guards..." << std::endl;
     for (unsigned i = 1; i < argc; i++) {
-        std::string file_name(argv[i]);
+        std::string_view file_name(argv[i]);
+        fs::path file_path(file_name);
         // Extract file name, if a path is given
 #ifdef _WIN32
         const std::size_t last_delim = file_name.find_last_of('\\');
-#else // _WIN32
+#else
         const std::size_t last_delim = file_name.find_last_of('/');
-#endif // _WIN32
+#endif
         if (last_delim != std::string::npos) {
             file_name = file_name.substr(last_delim + 1);
         }
-        // Open file
         std::cout << file_name << std::endl;
-        std::fstream file(argv[i], std::ios::out);
-        if (file.is_open()) {
-            // Write include guard
-            const std::string macro_name = get_macro_name(file_name);
-            file << "#ifndef " << macro_name << '\n';
-            file << "#define " << macro_name << '\n';
-            file << "#endif /* !" << macro_name << " */\n";
-            file.close();
+        // Check if the file exists
+        if (fs::exists(file_path)) {
+            // Open file
+            std::fstream file(argv[i], std::ios::out);
+            if (file.is_open()) {
+                // Write include guard
+                const std::string macro_name = get_macro_name(file_name);
+                file << "#ifndef " << macro_name << '\n';
+                file << "#define " << macro_name << '\n';
+                file << "#endif /* !" << macro_name << " */\n";
+                file.close();
+            } else {
+                std::cerr << " could not be opened" << std::endl;
+                ++num_fails;
+            }
         } else {
-            std::cerr << " could not be opened!" << std::endl;
+            std::cout << " does not exist" << std::endl;
             ++num_fails;
         }
     }
     std::cout << "Finished." << std::endl;
     if (num_fails) {
-        std::cout << num_fails << " error" << (num_fails == 1 ? "!" : "s!") << std::endl;
+        std::cout << num_fails << " error" << (num_fails == 1 ? ' ' : 's') << std::endl;
     }
     return 1;
 }
